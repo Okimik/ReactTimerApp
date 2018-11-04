2c226278b4c054e68c7946402a7beab661c36930
Pedro Chaves <chavespedr@gmail.com>
2018-11-04 22:34:51 +0000

    Initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,34 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+
+var Nav = () => {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">
+            React Timer App
+          </li>
+            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+          <li>
+            <Link to="/" activeClassName="active-link">Countdown</Link>
+          </li>
+          <li>
+          </li>
+
+
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">
+            Created by <a href="https://github.com/Okimik" target="_blank">Pedro Chaves</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+module.exports = Nav;
